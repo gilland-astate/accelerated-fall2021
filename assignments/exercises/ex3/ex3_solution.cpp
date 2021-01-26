d019203e873ee3c7e10ae1d45d82f7706e22776d
Steve Gilland <steven.gilland@gmail.com>
2021-01-26 16:54:36 -0600

    Changed ex3 Solution

--- assignments/exercises/ex3/ex3_solution.cpp
+++ assignments/exercises/ex3/ex3_solution.cpp
@@ -4,19 +4,33 @@ using std::cout;
 using std::cin;
 
 int main () {
+    //    4 exam scores (as integers)
     int exam1, exam2, exam3, exam4;
     cout << "Enter the 4 exam scores (seperated by spaces or new lines): ";
     cin >> exam1 >> exam2 >> exam3 >> exam4;
     
+    //    4 points possible for the exams (as integers)
     cout << "Enter the points possible for the 4 exams (seperated by spaces or new lines): ";
     int points1, points2, points3, points4;
     cin >> points1 >> points2 >> points3 >> points4;
 
     int examTotal, pointsTotal;
+    //    Total points of combined exams (as integer)
     examTotal = exam1 + exam2 + exam3 + exam4;
+    //    Total points possible of combined exams (as integer)
     pointsTotal = points1 + points2 + points3 + points4;
 
     cout << "The total points scored on all exams was " << examTotal << " out of a possible total of "
          << pointsTotal << ". Making the overall grade average " << std::fixed << std::setprecision(1)
          << static_cast<double>(examTotal) / pointsTotal * 100 << "%." << std::endl;
+    //      ^^^^^^^^^^^^^^^^^^^^^^^^^^
+    //    The average of the exams (as decimal with 1 place)
 }
+
+
+
+
+
+
+
+
