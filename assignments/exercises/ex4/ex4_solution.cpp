cbad674db2ec5b4f852006ee01ff727faa58c376
Steve Gilland <steven.gilland@gmail.com>
2021-01-27 16:20:31 -0600

    Upload ex4

--- assignments/exercises/ex4/ex4_solution.cpp
+++ assignments/exercises/ex4/ex4_solution.cpp
@@ -1,11 +1,34 @@
+#include <iostream>
 #include <fstream>
+#include <iomanip>
+using std::cout;
+using std::cin;
 
-int main() {
-    std::ofstream outfile{"output.txt"};
+int main () {
+    //    4 exam scores (as integers)
+    int exam1, exam2, exam3, exam4;
+    cout << "Enter the 4 exam scores (seperated by spaces or new lines): ";
+    cin >> exam1 >> exam2 >> exam3 >> exam4;
+    
+    //    4 points possible for the exams (as integers)
+    cout << "Enter the points possible for the 4 exams (seperated by spaces or new lines): ";
+    int points1, points2, points3, points4;
+    cin >> points1 >> points2 >> points3 >> points4;
 
-    outfile << "Hello, world!";
+    int examTotal, pointsTotal;
+    //    Total points of combined exams (as integer)
+    examTotal = exam1 + exam2 + exam3 + exam4;
+    //    Total points possible of combined exams (as integer)
+    pointsTotal = points1 + points2 + points3 + points4;
 
+    // Open the file to begin output
+    std::ofstream outfile{"grades.txt"};
+    if( !outfile )
+        std::exit(1);
+
+    outfile << exam1 << " " << exam2 << " " << exam3 << " " << exam4 << std::endl
+            << points1 << " " << points2 << " " << points3 << " " << points4 << std::endl
+            << std::fixed << std::setprecision(1)
+            << static_cast<double>(examTotal)/pointsTotal * 100 << "%" << std::endl;
     outfile.close();
-
-    return 0;
 }
